d4d5258de1832d202b7b68d71a43a4f128cdeebf
Emmi Sallinen <verijuotikas@winglet.(none)>
2013-11-02 14:58:36 +0200

    aloitetaan

---
+++ KillerPlanes/src/pelimoottori/KillerPlanes.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pelimoottori;
+
+import kayttaja.Pelaaja;
+import java.util.Scanner;
+
+/**
+ *
+ * @author emmisall
+ */
+public class KillerPlanes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //taulukko täällä, 2 pelaajaa lisätään
+        
+        Pelaaja[] pelaajat = new Pelaaja[2];
+        Scanner lukija = new Scanner(System.in);
+      
+        System.out.println("Anna pelaajanimesi:");
+        
+
+       
+    }
+}
